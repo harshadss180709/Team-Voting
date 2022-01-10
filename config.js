5122f72a8d536484953b06fde953c8b55abc7aae
harshadss180709 <97423648+harshadss180709@users.noreply.github.com>
2022-01-10 10:39:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
+const firebaseConfig = {
+    apiKey: "AIzaSyBqojKeC8g4N-VjY8C9-AC4s4WpgiqAJ5w",
+    authDomain: "voting-app-c7f6c.firebaseapp.com",
+    projectId: "voting-app-c7f6c",
+    storageBucket: "voting-app-c7f6c.appspot.com",
+    messagingSenderId: "193986236353",
+    appId: "1:193986236353:web:c74a112bfe394cd340e940"
+  };
 
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
 export default firebase.database();
